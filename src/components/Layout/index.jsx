fdfd40081ab83af170689d597f11c00c65853064
Ruben E Acosta <reacosta@grupo-exito.com>
2023-10-12 21:20:36 -0500

    first commit

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import IconMenu from '../../images/menu'
+import IconClose from '../../images/close'
+import './styles.css'
+
+export default function Layout({children, title}) {
+  const [showNav, setShowNav] = useState(false)
+  return (
+    <div className='layout'>
+      <header>
+        <button
+          type='button'
+          className='button_layout'
+          onClick={() => setShowNav(true)}
+        >
+          <IconMenu />
+          <span>Menu</span>
+        </button>
+        <h1
+          className='title_layout'
+          onClick={() => setShowNav(false)}
+        >{title}</h1>
+      </header>
+      <nav
+        style={showNav ? {left: '0'} : {left: '-80dvw'}} >
+        <div>
+          <p>Menu</p>
+          <button type='button' className='btnClose' onClick={() => setShowNav(false)}>
+            <IconClose />
+          </button>
+        </div>
+        <ul className='lista_nav'>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Ruta 1</a>
+          </li>
+          <li>
+            <a href="/">Ruta 2</a>
+          </li>
+          <li>
+            <a href="/">Ruta en texto</a>
+          </li>
+          <li>
+            <a href="/">Lugares</a>
+          </li>
+          <li>
+            <a href="/">Equipo</a>
+          </li>
+        </ul>
+      </nav>
+      <section
+        onClick={() => setShowNav(false)}
+        className='content'
+      >{children}</section>
+      <footer>
+        Create by <a
+        href='https://github.com/Byhako'
+        rel="noreferrer"
+        target='_blank'
+      >Byhako</a>
+      </footer>
+    </div>
+  )
+}
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import Layout from '../../components/Layout'
+import escudo from '../../images/escudo.png'
+
+export default function Home() {
+  return (
+    <Layout title='Conquistando Suramerica'>
+      <img src={escudo} alt="escudo" style={{ scale: '0.6' }}/>
+    </Layout>
+  )
+}
