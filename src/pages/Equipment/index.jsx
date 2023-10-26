47a2b82c215906de34349d8e7123f78e6f1fb797
Ruben E Acosta <reacosta@grupo-exito.com>
2023-10-26 16:31:31 -0500

    equipo

--- src/pages/Equipment/index.jsx
+++ src/pages/Equipment/index.jsx
@@ -1,68 +1,106 @@
+import { useState, useEffect } from 'react'
 import Layout from "../../components/Layout";
 import './styles.css'
 
 export default function Equipment() {
+  const [state, setState] = useState({
+    scales1: false,
+    scales2: false,
+    scales3: false,
+    scales4: false,
+    scales5: false,
+    scales6: false,
+    scales7: false,
+    scales8: false,
+    scales9: false,
+    scales10: false,
+    scales11: false,
+    scales12: false,
+    scales13: false,
+    scales14: false,
+  })
+
+  const handleClic = (key) => {
+    const newState = { ...state }
+    newState[key] = !newState[key]
+    setState(newState)
+    localStorage.setItem('checkEquipo', JSON.stringify(newState))
+  }
+
+  useEffect(() => {
+    const data = localStorage.getItem('checkEquipo')
+    if (data) {
+      setState(JSON.parse(data))
+    }
+  }, [])
+
   return (
     <Layout title='Equipo 🔧'>
-      <h2 className="title_equi">Obligatorio</h2>
-      <ul>
-        <li>
-          <input type="checkbox" id="scales" name="scales" checked />
-          <label for="scales">Chaqueta de proteccion, guantes, botas y rodilleras.</label>
-        </li>
-        <li>
-          <input type="checkbox" id="scales" name="scales" checked />
-          <label for="scales">Carpa y cobija.</label>
-        </li>
-        <li>
-          <input type="checkbox" id="scales" name="scales" checked />
-          <label for="scales">Recipiente para cargar agua.</label>
-        </li>
-        <li>
-          <input type="checkbox" id="scales" name="scales" checked />
-          <label for="scales">Conector USB en la moto.</label>
-        </li>
-        <li>
-          <input type="checkbox" id="scales" name="scales" checked />
-          <label for="scales">Guayas de repuesto. Acelerador, freno y clutch.</label>
-        </li>
-        <li>
-          <input type="checkbox" id="scales" name="scales" checked />
-          <label for="scales">Neumaticos de repuesto. 1 llanta delantera y 1 trasera.</label>
-        </li>
-        <li>
-          <input type="checkbox" id="scales" name="scales" checked />
-          <label for="scales">Herramienta basica de la moto. Llaves para desmontar las ruedas.</label>
-        </li>
-        <li>
-          <input type="checkbox" id="scales" name="scales" checked />
-          <label for="scales">2 galonesp para combustible de 5 litros.</label>
-        </li>
-      </ul>
-      
-      <h2 className="title_equi">Opcional</h2>
-      <ul>
-        <li>
-          <input type="checkbox" id="scales" name="scales" checked />
-          <label for="scales">Aceite para lubricar cadena.</label>
-        </li>
-        <li>
-          <input type="checkbox" id="scales" name="scales" checked />
-          <label for="scales">Inflador.</label>
-        </li>
-        <li>
-          <input type="checkbox" id="scales" name="scales" checked />
-          <label for="scales">Exploradoras moto.</label>
-        </li>
-        <li>
-          <input type="checkbox" id="scales" name="scales" checked />
-          <label for="scales">Botiquín básico.</label>
-        </li>
-        <li>
-          <input type="checkbox" id="scales" name="scales" checked />
-          <label for="scales">Bateria cargador portatil.</label>
-        </li>
-      </ul>
+      <div className='equipo'>
+        <h2 className="title_equi">Obligatorio</h2>
+        <ul>
+          <li>
+            <input type="checkbox" id="scales1" name="scales1" checked={state.scales1} onClick={() => handleClic('scales1')} />
+            <label for="scales1">Chaqueta de protección, guantes, botas y rodilleras.</label>
+          </li>
+          <li>
+            <input type="checkbox" id="scales2" name="scales2" checked={state.scales2} onClick={() => handleClic('scales2')} />
+            <label for="scales2">Carpa y cobija.</label>
+          </li>
+          <li>
+            <input type="checkbox" id="scales3" name="scales3" checked={state.scales3} onClick={() => handleClic('scales3')} />
+            <label for="scales3">Recipiente para cargar agua.</label>
+          </li>
+          <li>
+            <input type="checkbox" id="scales4" name="scales4" checked={state.scales4} onClick={() => handleClic('scales4')} />
+            <label for="scales4">Conector USB en la moto.</label>
+          </li>
+          <li>
+            <input type="checkbox" id="scales5" name="scales5" checked={state.scales5} onClick={() => handleClic('scales5')} />
+            <label for="scales5">Guayas de repuesto. Acelerador, freno y clutch.</label>
+          </li>
+          <li>
+            <input type="checkbox" id="scales6" name="scales6" checked={state.scales6} onClick={() => handleClic('scales6')} />
+            <label for="scales6">Neumáticos de repuesto. 1 llanta delantera y 1 trasera.</label>
+          </li>
+          <li>
+            <input type="checkbox" id="scales7" name="scales7" checked={state.scales7} onClick={() => handleClic('scales7')} />
+            <label for="scales7">Herramienta basica de la moto. Llaves para desmontar las ruedas.</label>
+          </li>
+          <li>
+            <input type="checkbox" id="scales8" name="scales8" checked={state.scales8} onClick={() => handleClic('scales8')} />
+            <label for="scales8">2 galones para combustible de 5 litros.</label>
+          </li>
+        </ul>
+        
+        <h2 className="title_equi">Opcional</h2>
+        <ul>
+          <li>
+            <input type="checkbox" id="scales9" name="scales9" checked={state.scales9} onClick={() => handleClic('scales9')} />
+            <label for="scales9">Aceite para lubricar cadena.</label>
+          </li>
+          <li>
+            <input type="checkbox" id="scales10" name="scales10" checked={state.scales10} onClick={() => handleClic('scales10')} />
+            <label for="scales10">Inflador.</label>
+          </li>
+          <li>
+            <input type="checkbox" id="scales11" name="scales11" checked={state.scales11} onClick={() => handleClic('scales11')} />
+            <label for="scales11">Exploradoras moto.</label>
+          </li>
+          <li>
+            <input type="checkbox" id="scales12" name="scales12" checked={state.scales12} onClick={() => handleClic('scales12')} />
+            <label for="scales12">Botiquín básico.</label>
+          </li>
+          <li>
+            <input type="checkbox" id="scales13" name="scales13" checked={state.scales13} onClick={() => handleClic('scales13')} />
+            <label for="scales13">Bateria cargador portatil.</label>
+          </li>
+          <li>
+            <input type="checkbox" id="scales14" name="scales14" checked={state.scales14} onClick={() => handleClic('scales14')} />
+            <label for="scales14">Cuerda.</label>
+          </li>
+        </ul>
+      </div>
     </Layout>
   )
 }
